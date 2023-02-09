79f0ae90ab0bb3dd4aa4b33860b989510137aa8f
Pedro Abreu <pvictor@novakio.com.br>
2023-02-09 15:35:17 -0300

    dia 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Curso-angular';
+  userName = 'Pedro';
+  userData = {
+    name : 'Pedro',
+    email : 'pedro.teste@gmail.com'
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './pages/first-component/first-component.component';
+import { ParentDataComponent } from './pages/parent-data/parent-data.component';
+import { DirectivesComponent } from './pages/directives/directives.component';
+import { IfRenderComponent } from './pages/if-render/if-render.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    ParentDataComponent,
+    DirectivesComponent,
+    IfRenderComponent
   ],
   imports: [
     BrowserModule,
